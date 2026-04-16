582a039a9eb2c7d77c15f64e515aef1365d774e9
rohitsomuri9-wq <rohit.somuri9@gmail.com>
2026-04-16 19:46:15 +0530

    added newline char

--- q2/q2.s
+++ q2/q2.s
@@ -1,5 +1,6 @@
 .section .data
-print: .string "%ld "           # format
+print: .string "%d "           # format
+newline: .string "\n"
 .section .text
 .global main
 .extern atoi
@@ -12,79 +13,80 @@ sd x11, 32(sp)              # save argv
 sd x20, 24(sp)              # save x20
 sd x21, 16(sp)              # save x21
 sd x22, 8(sp)               # save x22
-addi x6, x10, -1            # x6 = n
+mv x23, x11                 # x23 is used to store argv
+addi x6, x10, -1            # x6 = number of elements (n)
 addi sp, sp, -6000          # sp = sp - 6000
 addi x20, sp, 0             # x20 = arr
 addi x21, sp, 2000          # x21 = result
 addi x22, sp, 4000          # x22 = stack
-addi x7, x0, -1             # x7 = -1
-addi x5, x0, 0              # x5 = 0
+addi x7, x0, -1             # x7 = top = -1
+addi x5, x0, 0              # x5 = i = 0
 read_loop:
 beq x5, x6, read_done       # if i == n
-ld x11, 6032(sp)            # load argv
+mv x11, x23                 # load argv
 addi x9, x5, 1              # x9 = i+1
 slli x9, x9, 3              # x9 = (i+1)*8
 add x9, x11, x9             # x9 = argv[i+1]
 ld x10, 0(x9)               # x10 = string
-call atoi                   # atoi
-slli x10, x10, 32           # shift left
-srai x10, x10, 32           # shift right
-slli x9, x5, 3              # x9 = i*8
+call atoi                   # convert string to integer
+slli x9, x5, 2              # x9 = i*4
 add x9, x20, x9             # x9 = arr+i
-sd x10, 0(x9)               # arr[i] = x10
+sw x10, 0(x9)               # arr[i] = x10
 addi x5, x5, 1              # i = i+1
 beq x0, x0, read_loop       # goto read_loop
 read_done:
 addi x5, x6, -1             # x5 = n-1
 loop_i:
 blt x5, x0, end             # if i < 0
-slli x9, x5, 3              # x9 = i*8
+slli x9, x5, 2              # x9 = i*4
 add x9, x20, x9             # x9 = arr+i
-ld x8, 0(x9)                # x8 = arr[i]
+lw x8, 0(x9)                # x8 = arr[i]
 while:
 blt x7, x0, done_while      # if top < 0
-slli x9, x7, 3              # x9 = top*8
+slli x9, x7, 2              # x9 = top*4
 add x9, x22, x9             # x9 = stack+top
-ld x12, 0(x9)               # x12 = index
-slli x13, x12, 3            # x13 = index*8
+lw x12, 0(x9)               # x12 = index
+slli x13, x12, 2            # x13 = index*4
 add x13, x20, x13           # x13 = arr+index
-ld x13, 0(x13)              # x13 = arr[index]
-ble x13, x8, pop            # if <=
+lw x13, 0(x13)              # x13 = arr[index]
+ble x13, x8, pop            # if <= pop
 beq x0, x0, done_while      # else break
 pop:
 addi x7, x7, -1             # top = top-1
 beq x0, x0, while           # goto while
 done_while:
-slli x9, x5, 3              # x9 = i*8
+slli x9, x5, 2              # x9 = i*4
 add x9, x21, x9             # x9 = result+i
 blt x7, x0, no_elem         # if top < 0
-slli x12, x7, 3             # x12 = top*8
+slli x12, x7, 2             # x12 = top*4
 add x12, x22, x12           # x12 = stack+top
-ld x12, 0(x12)              # x12 = stack[top]
-sd x12, 0(x9)               # result[i] = x12
+lw x12, 0(x12)              # x12 = stack[top]
+sw x12, 0(x9)               # result[i] = x12
 beq x0, x0, push            # goto push
 no_elem:
 addi x12, x0, -1            # x12 = -1
-sd x12, 0(x9)               # result[i] = -1
+sw x12, 0(x9)               # result[i] = -1
 push:
 addi x7, x7, 1              # top = top+1
-slli x9, x7, 3              # x9 = top*8
+slli x9, x7, 2              # x9 = top*4
 add x9, x22, x9             # x9 = stack+top
-sd x5, 0(x9)                # stack[top] = i
+sw x5, 0(x9)                # stack[top] = i
 addi x5, x5, -1             # i = i-1
 beq x0, x0, loop_i          # goto loop_i
 end:
 addi x5, x0, 0              # x5 = 0
 print_loop:
 beq x5, x6, finish          # if i == n
-slli x9, x5, 3              # x9 = i*8
+slli x9, x5, 2              # x9 = i*4
 add x9, x21, x9             # x9 = result+i
-ld x11, 0(x9)               # x11 = result[i]
+lw x11, 0(x9)               # x11 = result[i]
 la x10, print               # x10 = format
 call printf                 # printf
 addi x5, x5, 1              # i = i+1
 beq x0, x0, print_loop      # goto print_loop
 finish:
+la x10, newline             # print newline
+call printf
 addi sp, sp, 6000           # sp = sp + 6000
 ld x22, 8(sp)               # load x22
 ld x21, 16(sp)              # load x21
